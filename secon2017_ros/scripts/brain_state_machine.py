62517d8ab791647fe7e5dd53ae0ceaf7b6f857b5
Lucas Cagle <ldc290@msstate.edu>
2016-10-18 12:04:27 -0500

    Added file for Brain State Machine Node. #2

---
+++ secon2017_ros/scripts/brain_state_machine.py
@@ -0,0 +1,10 @@
+#!/bin/env python
+
+'''
+This node acts as the state machine controller for Brain and the entire
+robotic system.
+
+It additionally uses dynamic reconfigure to change and adjust ROS and Arduino
+parameters during operation, allowing rapid development.
+
+'''
