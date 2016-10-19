154fd6017595a905733b2e67d27e730ed5fd7558
Lucas Cagle <ldc290@msstate.edu>
2016-10-18 19:10:45 -0500

    Began populating nodes. #2

--- secon2017_ros/scripts/brain_state_machine.py
+++ secon2017_ros/scripts/brain_state_machine.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 '''
 This node acts as the state machine controller for Brain and the entire
@@ -8,3 +8,84 @@ It additionally uses dynamic reconfigure to change and adjust ROS and Arduino
 parameters during operation, allowing rapid development.
 
 '''
+
+import rospy
+
+from dynamic_reconfigure.server import Server
+from secon2017_ros.cfg import BrainStateMachinConfig as BSMCfg
+
+
+class BrainStateMachine():
+
+    def __init__(self):
+
+        # Initialize node
+        rospy.init_node("brain_state_machine")
+        # Tracks current state
+        self.current_state = 0
+        # Lists all states of Brain
+        self.states = \
+        [
+            "wait_for_start",
+            "start",
+
+            "nav_to_STG1_wall",
+            "nav_to_STG1",
+            "align_to_STG1",
+            "perform_STG1",
+
+            "nav_to_STG3_wall",
+            "nav_to_STG3",
+            "align_to_STG3",
+            "perform_STG3",
+
+            "end"
+        ]
+
+        # Load parameter defaults here
+
+        # Start dynamic reconfigure server
+        self.dynamic_server = Server(BSMCfg, self.reconfigure_callback)
+
+        # Start state machine loop
+        self.run()
+
+    def run(self):
+        rate = rospy.rate(self.rate)
+        while not rospy.is_shutdown():
+            self.generate_state_commands()
+            rate.sleep()
+
+    def generate_state_commands(self):
+        # Sends commands based on current state
+        if self.states[self.current_state] == "wait_for_start":
+            return
+        elif self.states[self.current_state] == "start":
+            return
+        elif self.states[self.current_state] == "nav_to_STG1_wall":
+            return
+        elif self.states[self.current_state] == "nav_to_STG1":
+            return
+        elif self.states[self.current_state] == "align_to_STG1":
+            return
+        elif self.states[self.current_state] == "perform_STG1":
+            return
+        elif self.states[self.current_state] == "nav_to_STG3_wall":
+            return
+        elif self.states[self.current_state] == "nav_to_STG3":
+            return
+        elif self.states[self.current_state] == "align_to_STG3":
+            return
+        elif self.states[self.current_state] == "perform_STG3":
+            return
+        elif self.states[self.current_state] == "end":
+            return
+
+    def reconfigure_callback(self, config, level):
+        # Reassigns and resends parameters to Arduino's
+        return
+
+    def robot_state_callback(self, robot_state):
+        # Updates info dictionary of robot state information
+        # Trips a flag for new info
+        return
--- secon2017_ros/scripts/embedded_interface.py
+++ secon2017_ros/scripts/embedded_interface.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 '''
 This node handles serial communications with both Arduino devices of the robotic
@@ -7,3 +7,65 @@ Brain State Machine and passed onto the appropriate Arduino.
 
 Sensor data is also forwarded up from the Arduino to the State Machine.
 '''
+
+import rospy
+import serial
+import regex as re
+
+
+class EmbeddedInterface():
+
+    def __init__():
+        # Initialize node
+        rospy.init_node("embedded_interface")
+        self.pinky_connected = True
+        # regex strings to extract data from strings
+        self.brain_regex = "B:sw:*[]odom*[]wvel:*[]seq:*[]rot:*[]"
+        self.pinky_regex = "P:"
+        # command string template
+        # x,y,angular velocities; stage 1, 3 triggers
+        self.command_string = "C{xvel},{yvel},{zangv};{STG1},{STG3}"
+        # parameter string template
+        self.parameter_string = "P{}"
+        # Wheel debug string
+        self.debug_string = "W{w1vel},{w2vel},{w3vel},{w4vel}"
+        # Set parameters
+        self.brain_port = rospy.get_param("brain_serial_port", "/dev/arduinos/brain")
+        self.pinky_port = rospy.get_param("pinky_serial_port", "/dev/arduinos/pinky")
+        # Set callbacks
+
+        # Open serial ports
+        self.Brain = serial.Serial(self.brain_port, self.brain_baud, timeout=0.5)
+        self.Pinky = serial.Serial(self.pinky_port, self.pinky_baud, timeout=0.5)
+
+        # Launch main listening loop
+        self.run()
+
+    def command_callback(self, command):
+        # Forwards command operations
+        return
+
+    def parameter_callback(self, parameters):
+        # Forwards parameter reassignments
+        return
+
+    def run(self):
+        # Main loop to read from serial ports
+        rate = rospy.Rate(self.rate)
+
+        while not rospy.is_shutdown():
+
+            brain_data = self.Brain.readline()
+            # Don't bother once disconnected from Pinky
+            if self.pinky_connected:
+                pinky_data = self.Pinky.readline()
+            # Parse the command
+            self.parse_state_string(brain_data)
+
+            self.parse_state_string(pinky_data)
+            rate.sleep()
+        return
+
+    def parse_state_string(self, state_str):
+        # Parses raw serial string into data and publishes it
+        return
